6614d5a3cee356178377d27121ec6f573281d82b
ckp1701 <euo886@gmail.com>
2017-10-27 12:26:05 +0200

    added sys.exit()

--- feederek.py
+++ feederek.py
@@ -4,13 +4,13 @@ import telegram
 import sys
 from time import sleep
 
-feed_list =["http://baikalinform.ru/obyavki-rss",
+feed_list =["https://yourfeed.com/feed/",
             ]
 
 last_feeds = pickle.load(open("db.p", 'rb'))
 fee_links = []
 
-bot = telegram.Bot(token='228955506:AAGmwy8-a7b5PKjuZjiPk_7OJ0kJ6LiHBvM')
+bot = telegram.Bot(token='TELEGRAM_BOT_TOKEN_HERE')
 
 print(last_feeds)
 print("-----Last feeds---")
@@ -24,14 +24,14 @@ def feederek():
             if fee['entries'][x]['id'] in last_feeds:
                 print("Nothing new - " + fee_title)
             else:
-                sleep(5)
+                sleep(5) # for server flood detection
                 entry_title = fee['entries'][x]['title']
                 entry_id = fee['entries'][x]['id']
                 print("Updated - " + fee_title)
 
 
                 message = str(fee_title +"\n" + entry_title +"\n" + entry_id)
-                bot.sendMessage(chat_id="@CyberSecPL", text=message)
+                bot.sendMessage(chat_id="@CHANNEL_NAME_HERE", text=message)
 
     pickle.dump(fee_links, open("db.p", 'wb'))
     sys.exit()
